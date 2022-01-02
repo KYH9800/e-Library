7e1f944c6dcdef31fced976cc7878fc224b0ae1a
고윤혁 <sksgur3217@gmail.com>
2022-01-03 06:14:51 +0900

    completed login/signup html

--- prepare/front/pages/login.js
+++ prepare/front/pages/login.js
@@ -2,39 +2,49 @@ import React from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 
+import { Emptyplace } from '../components/style/AppLayoutSt';
+
+import AppLayout from '../components/AppLayout';
+
 const Login = () => {
+  // todo
   return (
-    <form>
-      <Head>
-        <title>K-Style | 로그인</title>
-      </Head>
-      <div>
+    <AppLayout>
+      <form>
+        <Head>
+          <title>K-Style | 로그인</title>
+        </Head>
+        <Emptyplace />
         <div>
-          <Link href="/">
-            <a>e도서관</a>
-          </Link>
+          <div>로그인</div>
+          <div>
+            <label htmlFor="user-email">아이디</label>
+            <br />
+            <input type="email" required />
+          </div>
+          <div>
+            <label htmlFor="password">비밀번호</label>
+            <br />
+            <input type="password" required />
+          </div>
+          <div>
+            <button type="submit" htmlType="submit">
+              로그인
+            </button>
+          </div>
+          <div>
+            <div>
+              <Link href="/signup">
+                <a>회원가입</a>
+              </Link>
+            </div>
+            <div>
+              <a>아이디 찾기</a>
+            </div>
+          </div>
         </div>
-        <div>로그인</div>
-        <div>
-          <label htmlFor="user-email">아이디</label>
-          <br />
-          <input type="email" required />
-        </div>
-        <div>
-          <label htmlFor="password">비밀번호</label>
-          <br />
-          <input type="password" required />
-        </div>
-        <button type="submit" htmlType="submit">
-          로그인
-        </button>
-        <Link href="/signup">
-          <a>
-            <button>회원가입</button>
-          </a>
-        </Link>
-      </div>
-    </form>
+      </form>
+    </AppLayout>
   );
 };
 
--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -2,56 +2,54 @@ import React from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 
+import { Emptyplace } from '../components/style/AppLayoutSt';
+
+import AppLayout from '../components/AppLayout';
+
 const Signup = () => {
   // todo
-
   return (
-    <form>
-      <Head>
-        <title>K-Style | 회원가입</title>
-      </Head>
-      <div>
-        <Link href="/">
-          <a>e도서관</a>
-        </Link>
-      </div>
-      <div>회원가입</div>
-      <div>
-        <label htmlFor="user-nickname">닉네임</label>
-        <br />
-        <input type="nickname" name="user-nickname" required />
-      </div>
-      <div>
-        <label htmlFor="user-email">이메일</label>
-        <br />
-        <input type="email" name="user-email" required />
-      </div>
-      <div>
-        <label htmlFor="user-password">비밀번호</label>
-        <br />
-        <input type="password" name="user-password" required />
-      </div>
-      <div>
-        <label htmlFor="user-password-check">비밀번호 확인</label>
-        <br />
-        <input type="password" name="user-password-check" required />
-        <div>비밀번호가 일치하지 않습니다.</div>
-        <div>
-          <checkbox name="user-term">check: 관리자의 말을 잘 들을 것을 동의합니다.</checkbox>
-          <div>약관에 동의하셔야 합니다.</div>
-        </div>
-        <div>
-          <button type="submit" htmlType="submit">
-            가입하기
-          </button>
-          <button>
-            <Link href="/login">
-              <a>로그인</a>
-            </Link>
-          </button>
-        </div>
-      </div>
-    </form>
+    <AppLayout>
+      <>
+        <form>
+          <Head>
+            <title>K-Style | 회원가입</title>
+          </Head>
+          <Emptyplace />
+          <div>회원가입</div>
+          <div>
+            <label htmlFor="user-nickname">닉네임</label>
+            <br />
+            <input type="nickname" name="user-nickname" required />
+          </div>
+          <div>
+            <label htmlFor="user-email">이메일</label>
+            <br />
+            <input type="email" name="user-email" required />
+          </div>
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <input type="password" name="user-password" required />
+          </div>
+          <div>
+            <label htmlFor="user-password-check">비밀번호 확인</label>
+            <br />
+            <input type="password" name="user-password-check" required />
+            <div>비밀번호가 일치하지 않습니다.</div>
+            <div>
+              <checkbox name="user-term">check: 관리자의 말을 잘 들을 것을 동의합니다.</checkbox>
+              <div>약관에 동의하셔야 합니다.</div>
+            </div>
+            <div>
+              <button type="submit" htmlType="submit">
+                가입하기
+              </button>
+            </div>
+          </div>
+        </form>
+      </>
+    </AppLayout>
   );
 };
 
