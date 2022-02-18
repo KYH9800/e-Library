79329b6e38cb8528c16f8a39ab06a287b08405a2
고윤혁 <sksgur3217@gmail.com>
2022-02-19 00:24:34 +0900

    change a back url

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,2 +1,2 @@
 // export const backURL = 'http://localhost:3065';
-export const backURL = process.env.NODE_ENV === 'production' ? 'http://13.124.161.143' : 'http://localhost:3065';
+export const backURL = process.env.NODE_ENV === 'production' ? 'http://http://13.209.21.186/' : 'http://localhost:3065';
