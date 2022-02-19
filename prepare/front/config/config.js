8b61942e9708b6b2388178a7cbd4f38091bb90f4
고윤혁 <sksgur3217@gmail.com>
2022-02-19 21:51:54 +0900

    vercel setting

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,3 +1,3 @@
 // export const backURL = 'http://localhost:3065';
 // export const backURL = 'http://13.209.21.186';
-export const backURL = process.env.NODE_ENV === 'production' ? 'http://13.209.21.186' : 'http://localhost:3065';
+export const backURL = process.env.NODE_ENV === 'development' ? 'http://13.209.21.186' : 'http://localhost:3065';
