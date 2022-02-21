c535c0f9fc302dbff661b9d2b4bda1d4a0395f6c
고윤혁 <sksgur3217@gmail.com>
2022-02-21 17:18:10 +0900

    add nginx + https

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,4 +1,4 @@
 // export const backURL = 'http://localhost:3065';
 // export const backURL = 'http://13.209.21.186';
 export const backURL =
-  process.env.NODE_ENV === 'production' ? 'http://api.coding-factory.site' : 'http://localhost:3065';
+  process.env.NODE_ENV === 'production' ? 'https://api.coding-factory.site' : 'http://localhost:3065';
